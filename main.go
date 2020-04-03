25c7cb6e24b8d6b80d4654896071910151cfb781
Camelva <zaharkevych@yandex.ru>
2020-04-03 19:05:56 +0300

    Completely rewritten. Now whole url-processing logic moved to external library. There is only telegram-related stuff like, handling messages and response.

--- main.go
+++ main.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"regexp"
 
+	"github.com/camelva/erzo"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/user/tgbot/scdownloader"
 )
-
-const (
-	MsgNotSoundCloud   string = "Please send me a message with valid SoundCloud url or type /help for more info"
-	MsgError           string = "Sorry, but there is some problems with this song. Please try another one or contact with developer"
-	MsgPlaylist        string = "Sorry, but i don't work with playlists yet. Type /help for more info"
-	MsgDownloadingSong string = "Please wait, i'm downloading this song..."
-	MsgUploadingToUser string = "Everything done. Uploading song to you..."
-	MsgCommandHelp     string = "Send me an url and i will respond to you with attached audio.\n" +
-		"Playlists not supported yet\n\n" +
-		"If something went wrong - try again or contact with developer (link in description)"
-	MsgCommandStart string = "Hello, #{username}.\n" +
-		"I'm SoundCloud downloader bot.\n" +
-		"Send me an url and i will respond with attached audio file"
-	MsgCommandUnknown string = "I don't know that command. " +
-		"Please send me an url to SoundCloud song or type /help for more info"
-)
-
-var cfg Config
 
 func main() {
-
-	cfg = loadConfig("config.yml")
-	if (cfg == Config{}) {
-		log.Fatal("Can't load config")
-	}
-	bot, err := tgbotapi.NewBotAPI(cfg.Telegram.Token)
+	config := loadConfig("config.yml")
+	analytics := NewAnalytics(config.Segmentio.Token)
+	//noinspection GoUnhandledErrorResult
+	defer analytics.client.Close()
+	bot, err := tgbotapi.NewBotAPI(config.Telegram.Token)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -49,118 +27,135 @@ func main() {
 	updates, err := bot.GetUpdatesChan(u)
 
 	for update := range updates {
+		var msg *tgbotapi.Message
 		if update.Message != nil {
-			if err := receivedMessage(bot, update.Message); err != nil {
-				pingError(err, bot, update.Message.Chat.ID)
-			}
-			continue
+			msg = update.Message
 		} else if update.ChannelPost != nil {
-			if err := receivedMessage(bot, update.ChannelPost); err != nil {
-				reportError(err)
-			}
-			continue
+			msg = update.ChannelPost
 		} else {
 			continue
+		}
+		analytics.Identify(msg)
+		analytics.NewMessage(msg)
+		if err := handleMessage(bot, msg); err != nil {
+			handleError(bot, msg, err, analytics)
 		}
 	}
 }
 
-func pingError(e error, bot *tgbotapi.BotAPI, chatID int64) {
-	sendMessage(bot, chatID, MsgError)
-	reportError(e)
-	return
+func handleError(bot *tgbotapi.BotAPI, msg *tgbotapi.Message, err error, analytics Analytics) {
+	var responseMsg string
+	switch err.(type) {
+	case erzo.ErrNotURL:
+		responseMsg = BotPhrase.ErrNotURL
+		err = nil // its not really an error, dont need to report
+		log.Println("Received message without link. Responding...")
+	case erzo.ErrUnsupportedService:
+		responseMsg = BotPhrase.ErrUnsupportedService
+		err = nil // its not really an error, dont need to report
+		log.Println("Received message with link from unsupported service. Responding...")
+	case erzo.ErrUnsupportedProtocol:
+		// almost similar for user but we need to report about it
+		responseMsg = BotPhrase.ErrUnsupportedService
+	case erzo.ErrUnsupportedType:
+		if err.(erzo.ErrUnsupportedType).Format == "playlist" {
+			responseMsg = BotPhrase.ErrPlaylist
+		} else {
+			responseMsg = BotPhrase.ErrUnsupportedFormat
+		}
+		err = nil // its not really an error, dont need to report
+		log.Println("Received message with unsupported url type. Responding...")
+	case erzo.ErrCantFetchInfo:
+		// most of the time, can't fetch if song is unavailable, and that's what we respond to user
+		// we don't really need to report this error to analytic, but lets keep it for more verbose
+		responseMsg = BotPhrase.ErrUnavailableSong
+	case erzo.ErrDownloadingError:
+		// it means we fetched all info, but could not download it. Tell user to try again
+		responseMsg = BotPhrase.ErrUndefined
+	case erzo.ErrUndefined:
+		responseMsg = BotPhrase.ErrUndefined
+	default:
+		responseMsg = BotPhrase.ErrUndefined
+	}
+	if err != nil && err.Error() == "Request Entity Too Large" {
+		responseMsg = "Looks like this song weighs too much.\n" +
+			"Telegram limits uploading files size to 50mb and we can't avoid this limit.\n" +
+			"Please try another one"
+		err = nil // its not error, dont report
+	}
+
+	if err != nil {
+		analytics.NewError(msg, err)
+	}
+
+	if msg.Chat.Type != "private" {
+		return
+	}
+
+	sendMessage(bot, msg.Chat.ID, responseMsg)
 }
 
-func receivedMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) error {
-	analytics(message)
-	chatType := message.Chat.Type
-	private := chatType == "private"
+func handleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) error {
+	var isPrivateChat = message.Chat.Type == "private"
 	var tmpMessageID int
-	//log.Println("Received new message", message.Text)
-	chatID := message.Chat.ID
+	var chatID = message.Chat.ID
+
+	log.Println("Received new message", message.Text)
+
 	if message.IsCommand() {
-		log.Println("Received command -", message.Text, ".Responding...")
 		response := checkForCommands(message)
 		sendMessage(bot, chatID, response)
 		return nil
 	}
-	rawURL, status := getSCLink(message.Text)
-	if status != 0 {
-		if private {
-			if status == 1 {
-				log.Println("Received message without link in private chat. Responding...")
-				sendMessage(bot, chatID, MsgNotSoundCloud)
-			} else if status == 2 {
-				log.Println("Received message with playlist url in private chat. Responding...")
-				sendMessage(bot, chatID, MsgPlaylist)
+
+	if isPrivateChat {
+		tmpMessageID = sendMessage(bot, chatID, BotPhrase.ProcessStart)
+	}
+	defer deleteTempMessage(bot, message.Chat, tmpMessageID)
+
+	songFile, err := erzo.Get(message.Text, erzo.Truncate(true))
+	if err != nil {
+		if !isPrivateChat {
+			msgToDelete := tgbotapi.NewDeleteMessage(chatID, tmpMessageID)
+			if _, err := bot.DeleteMessage(msgToDelete); err != nil {
+				log.Printf("error while deleting temp message: %s", err)
 			}
 		}
-		return nil
-	}
-	if private {
-		tmpMessageID = sendMessage(bot, chatID, MsgDownloadingSong)
-	}
-	log.Println("Received message with soundcloud url. Downloading song...")
-	songFile, err := scdownloader.Download(rawURL, cfg.SoundCloud.Token)
-	if err != nil {
-		log.Printf("There is error while downloading: %s\n", err)
 		return err
 	}
 	log.Println("Downloaded song. Uploading to user...")
-	if private {
-		tmpMessageID = sendMessage(bot, chatID, MsgUploadingToUser, tmpMessageID)
+
+	if isPrivateChat {
+		// update old temp message
+		tmpMessageID = sendMessage(bot, chatID, BotPhrase.ProcessUploading, tmpMessageID)
 	}
+
 	// Inform user about uploading
-	if _, err := bot.Send(tgbotapi.NewChatAction(chatID, "upload_audio")); err != nil {
+	// but we don't care about possible errors
+	_, _ = bot.Send(tgbotapi.NewChatAction(chatID, "upload_audio"))
+
+	audioMsg := tgbotapi.NewAudioUpload(chatID, songFile)
+
+	// and only then send song file
+	if _, err := bot.Send(audioMsg); err != nil {
 		return err
 	}
-	audioU := tgbotapi.NewAudioUpload(chatID, songFile)
-	if _, err := bot.Send(audioU); err != nil {
-		return err
-	}
-	if private {
-		msgToDelete := tgbotapi.NewDeleteMessage(chatID, tmpMessageID)
-		if _, err := bot.DeleteMessage(msgToDelete); err != nil {
-			return err
-		}
-	}
-	log.Println("Deleting file", songFile, "...")
-	deleteFile(songFile)
+
 	log.Println("Waiting for another message ~_~")
 	return nil
 }
 
 func checkForCommands(message *tgbotapi.Message) (response string) {
+	log.Println("Received command -", message.Text, ".Responding...")
+
 	switch message.Command() {
 	case "help":
-		response = MsgCommandHelp
+		return BotPhrase.CmdHelp
 	case "start":
-		response = MsgCommandStart
+		return BotPhrase.CmdStart
 	default:
-		response = MsgCommandUnknown
+		return BotPhrase.CmdUnknown
 	}
-	return response
-}
-
-// If failed - return empty string as <url>
-// Status codes: [0 - ok, 1 - not SC, 2 - playlist url]
-func getSCLink(message string) (url string, status int8) {
-	// old = `(http.?://)(m\.)?(soundcloud.com)/(\S+)/(\S+)(/\S+)?`
-	regStr := `(http[s]?://)?(m\.)?(soundcloud\.com/)([\w-+\.#;!]+/)([\w-+\.#;!]+)(/[\w-+\.#;!]+)?`
-	re := regexp.MustCompile(regStr)
-	// res contain array with result of regExp: [0] - full string,
-	// [1] - protocol, [2] - "m." if exist, [3] - domain + /, [4] - user + /,
-	// [5] - song (or "sets" if its playlist) [6] - / + playlist link
-	res := re.FindStringSubmatch(message)
-	if res == nil {
-		return "", 1
-	}
-	if res[5] == "sets" {
-		return "", 2
-	}
-	url = fmt.Sprintf("https://%s%s%s", res[3], res[4], res[5])
-	log.Printf("%+v", url)
-	return url, 0
 }
 
 func sendMessage(bot *tgbotapi.BotAPI, chatID int64, text string, oldMsgContainer ...int) (msgID int) {
@@ -172,17 +167,23 @@ func sendMessage(bot *tgbotapi.BotAPI, chatID int64, text string, oldMsgContaine
 	} else {
 		msgObj = tgbotapi.NewMessage(chatID, text)
 	}
-	sentMsg, err := bot.Send(msgObj)
-	if err != nil {
-		log.Panic(err)
-		return 0
+	// two tries
+	for range make([]int, 2) {
+		sentMsg, err := bot.Send(msgObj)
+		if err != nil {
+			continue
+		}
+		return sentMsg.MessageID
 	}
-	return sentMsg.MessageID
+	return 0
 }
 
-func deleteFile(name string) {
-	if err := os.Remove(name); err != nil {
-		log.Panic(err)
+func deleteTempMessage(bot *tgbotapi.BotAPI, chat *tgbotapi.Chat, messageID int) {
+	if chat.Type != "private" {
+		return
 	}
-	return
+	msgToDelete := tgbotapi.NewDeleteMessage(chat.ID, messageID)
+	if _, err := bot.DeleteMessage(msgToDelete); err != nil {
+		log.Printf("error while deleting temp message: %s", err)
+	}
 }
